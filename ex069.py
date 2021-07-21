449377a2ebd2db701c59cd96907721c521fe0664
NeroDev <nairondalmaso2017@gmail.com>
2021-07-21 17:40:46 -0400

    Correçao ex069 verificador faltando

--- ex069.py
+++ ex069.py
@@ -4,16 +4,20 @@ while True:
     print('CADASTRAR PESSOA')
     print('=' * 17, '\033[m')
     idade = int(input('\033[33mIdade: \033[m'))
-    sexo = str(input('\033[33mSexo: [F/M] \033[m')).strip().upper()[0]
+    sexo = ' '
+    while sexo not in 'FM':
+        sexo = str(input('\033[33mSexo: [F/M] \033[m')).strip().upper()[0]
     if idade >= 18:
         maior_18 += 1
     if sexo == 'M':
         homens += 1
     if sexo == 'F' and idade <= 20:
         mulheres_novas += 1
-    continuar = str(input('\033[31mCadastrar mais alguem? [S/N] \033[m')).strip().upper()[0]
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('\033[31mCadastrar mais alguem? [S/N] \033[m')).strip().upper()[0]
     if continuar == 'N':
-        break
+                break
 print(f'\033[32m{maior_18} pessoas são maiores de 18 anos.\033[m')
-print(f'\033[32m{homens} foram cadastrados no total.\033[m')
+print(f'\033[32m{homens} homens foram cadastrados no total.\033[m')
 print(f'\033[32m{mulheres_novas} mulheres com ou menos de 20 anos foram cadastrados.\033[m')
