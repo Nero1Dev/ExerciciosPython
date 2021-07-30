5bc87b0318a1568d81ad8c16ce98902497aeca2f
NeroDev <nairondalmaso2017@gmail.com>
2021-07-29 21:09:49 -0400

    correção ex073

--- ex073.py
+++ ex073.py
@@ -1,4 +1,8 @@
-tabela = ('Palmeiras', 'Atlético-MG', 'Fortaleza', 'Bragantino', 'Athletico-PR', 'Flamengo', 'Ceará', 'Atlético-GO', 'Bahia', 'Corinthians', 'Fluminense', 'Santos', 'Juventude', 'Internacional', 'Cuiabá', 'Sport', 'São Paulo', 'América-MG', 'Grêmio', 'Chapecoense')
+tabela = ('Palmeiras', 'Atlético-MG', 'Fortaleza', 'Bragantino',
+'Atlético-PR', 'Flamengo', 'Ceará', 'Atlético-GO', 
+'Bahia', 'Corinthians', 'Fluminense', 'Santos', 
+'Juventude', 'Internacional', 'Cuiabá', 'Sport', 
+'São Paulo', 'América-MG', 'Grêmio', 'Chapecoense')
 
 print('=-' * 35)
 print(f'Lista completa: {tabela}')
@@ -7,11 +11,11 @@ print('=-' * 35)
 print(f'Os 5 primeiros time da tabela são: {tabela[0:5]}')
 print('=-' * 35)
 
-print(f'Os 4 últimos times da tabela: {tabela[16:]}')
+print(f'Os 4 últimos times da tabela: {tabela[-4:]}')
 print('=-' * 35)
 
 print(f'Os  times em ordem alfabética: {sorted(tabela)}')
 print('=-' * 35)
 
-print(f'na ultima colocação 20 {tabela[19]}')
+print(f'O Chapecoense esta na {tabela.index("Chapecoense") + 1}ª posição')
 print('=-' * 35)        
