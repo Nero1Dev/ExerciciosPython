e042afb7b4dcb61e0f3b69536cb7f8999ef0edf9
NeroDev <nairondalmaso2017@gmail.com>
2021-07-29 20:31:28 -0400

    ex072

---
+++ ex072.py
@@ -0,0 +1,5 @@
+números = ('zero', 'úm', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorse', 'quinze', 'dezesseis', 'desesete', 'dezoito', 'desenove', 'vinte')
+
+number = int(input('Digite um número: '))
+
+print(f'Você digitou {números[number-1]}')
