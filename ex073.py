dda14f8fdcf5ceebc104bf42af88d981cc0ac45b
NeroDev <nairondalmaso2017@gmail.com>
2021-07-29 20:54:36 -0400

    ex073

---
+++ ex073.py
@@ -0,0 +1,17 @@
+tabela = ('Palmeiras', 'Atlético-MG', 'Fortaleza', 'Bragantino', 'Athletico-PR', 'Flamengo', 'Ceará', 'Atlético-GO', 'Bahia', 'Corinthians', 'Fluminense', 'Santos', 'Juventude', 'Internacional', 'Cuiabá', 'Sport', 'São Paulo', 'América-MG', 'Grêmio', 'Chapecoense')
+
+print('=-' * 35)
+print(f'Lista completa: {tabela}')
+print('=-' * 35)
+
+print(f'Os 5 primeiros time da tabela são: {tabela[0:5]}')
+print('=-' * 35)
+
+print(f'Os 4 últimos times da tabela: {tabela[16:]}')
+print('=-' * 35)
+
+print(f'Os  times em ordem alfabética: {sorted(tabela)}')
+print('=-' * 35)
+
+print(f'na ultima colocação 20 {tabela[19]}')
+print('=-' * 35)        
