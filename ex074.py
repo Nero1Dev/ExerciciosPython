b119dbcc42a364e9a2133f202c07af2d86f9fcb4
NeroDev <nairondalmaso2017@gmail.com>
2021-07-29 21:26:46 -0400

    correção ex074

--- ex074.py
+++ ex074.py
@@ -3,10 +3,10 @@ from random import randint
 n = (randint(0, 10), randint(0, 10), randint(0, 10),
 randint(0, 10), randint(0, 10))
 
-print(f'Sorteei ps valores: ', end=' ')
+print(f'Sorteei os valores: ', end=' ')
 
 for c in n:
-    print(f'{c}', end='/')
+    print(f'{c}', end=' ')
 
 print(f'\nO maior número foi {max(n)}')
 print(f'O menor número foi {min(n)}')
