f5e15325794b7ab4bbdca960a5ef3c27de75bc45
NeroDev <nairondalmaso2017@gmail.com>
2021-07-30 17:23:08 -0400

    correção ex075

--- ex075.py
+++ ex075.py
@@ -12,5 +12,8 @@ for c in núm:
 
 print(núm)         
 print(f'O número 9 apareceu {núm.count(9)} vezes')
-print(f'O valor 3 foi digitado na {núm.index(3) + 1} posição')
+if 3 in núm:
+    print(f'O valor 3 foi digitado na {núm.index(3) + 1} posição')
+else:
+    print(f'O valor 3 não foi digitado em nenhuma posição')
 print(f'Você digitou {cont} número(s) pares')
