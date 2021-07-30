c2a541d08bf3769144e2ad8c0bcedbe10c7145dd
NeroDev <nairondalmaso2017@gmail.com>
2021-07-29 20:46:42 -0400

    correção + bonus

--- ex072.py
+++ ex072.py
@@ -1,5 +1,17 @@
-números = ('zero', 'úm', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorse', 'quinze', 'dezesseis', 'desesete', 'dezoito', 'desenove', 'vinte')
+números = ('zero', 'úm', 'dois', 'três', 'quatro', 
+'cinco', 'seis', 'sete', 'oito', 'nove',
+'dez', 'onze', 'doze', 'treze', 'quatorse',
+'quinze', 'dezesseis', 'desesete', 'dezoito',
+'desenove', 'vinte')
 
-number = int(input('Digite um número: '))
-
-print(f'Você digitou {números[number-1]}')
+while True:
+    while True:
+        number = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= number <= 20:
+            break
+        print('Tente Novamente. ', end=' ')
+    print(f'Você digitou {números[number]}')
+    pause = str(input('Você que continuar? [S/N] ')).strip().upper()[0]
+    if pause == 'N':
+        break
+    
