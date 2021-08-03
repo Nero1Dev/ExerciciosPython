f45502db53964e7862e114877c4d5deb759a81d0
NeroDev <nairondalmaso2017@gmail.com>
2021-08-03 16:57:14 -0400

    correção

--- ex078.py
+++ ex078.py
@@ -5,18 +5,26 @@ menos = 0
 for c in range(0, 5):
     valores.append(int(input(f'Digite um valor para posição {c}: ')))
 
-if valores[0] != mais or menos:
-    mais = valores[0]
-    menos = valores[0]
-for c in range(0, 5):
-    if valores[c] < menos:
-        menos = valores[c]
-    if valores[c] > mais:
-        mais = valores[c]
 
-# maislugar = valores.index(mais)
-# menoslugar = valores.index(menos)
+    if c == 0:
+        mais = menos = valores[c]
+    else:
+        if valores[c] < menos:
+            menos = valores[c]
+        if valores[c] > mais:
+            mais = valores[c]
+
+print('=-' * 30)
 
 print(f'Você digitou os valores {valores}')
-print(f'O menor valor digitado foi {menos}')
-print(f'O maior valor digitado foi {mais}')
+
+print(f'O menor valor digitado foi {menos} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == menos:
+        print(f'{i}... ', end='')
+print()
+print(f'O maior valor digitado foi {mais} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == mais:
+        print(f'{i}... ', end='')
+print()
