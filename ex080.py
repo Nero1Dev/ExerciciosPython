300296dc27e99d82fa340808ceb442f3bece850f
NeroDev <nairondalmaso2017@gmail.com>
2021-08-11 20:13:50 -0400

    correção

--- ex080.py
+++ ex080.py
@@ -1,7 +1,17 @@
-valores = list()
+valores = []
 
 for c in range(0, 5):
     valor = int(input('Digite um valor: '))
-    valores.append(valor)
-
-print(f'{valores})
+    if c == 0 or valor > valores[-1]:
+        valores.append(valor)
+        print('Adicionado ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(valores):
+            if valor <= valores[pos]:
+                valores.insert(pos, valor)
+                print(f'Adicionado a posição {pos} da lista...')
+                break
+            pos += 1
+print('=' * 53)
+print(f'Os valores digitados em ordem foram: {valores}')
