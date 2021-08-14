2b24f76fd4fa211721f4c8d370747665cc6c7a84
NeroDev <nairondalmaso2017@gmail.com>
2021-08-14 12:32:44 -0400

    ex081

---
+++ ex081.py
@@ -0,0 +1,12 @@
+lista = list()
+
+while True:
+    lista.append(int(input('Digite um valor: ')))
+    continuar = str(input('Quer adicionar mais números? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+
+print('=' * 50)
+print(f'Os valores digitador foram: {lista}')
+print(f'{len(lista)} Numero(s) foram digitados')
+print(f'Os números em ordem decrecentes fica: {sorted(lista, reverse=True)}')
