f1fb5d0249a6400203b87c557320a75134b11a84
NeroDev <nairondalmaso2017@gmail.com>
2021-08-15 18:59:25 -0400

    ex081

--- ex081.py
+++ ex081.py
@@ -10,4 +10,8 @@ print('=' * 50)
 print(f'Os valores digitador foram: {lista}')
 print(f'{len(lista)} Numero(s) foram digitados')
 print(f'Os números em ordem decrecente fica: {sorted(lista, reverse=True)}')
+if 5 in lista: 
+    print('A lista tem o número 5')
+else:
+    print('A lista não tem o 5')
 print('=' * 50)
