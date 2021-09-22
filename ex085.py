1753f4acee55b415cf9f986fdd2238f303fe2810
Nero1Dev <nairondalmaso2017@gmail.com>
2021-09-21 21:36:28 -0300

    ex085

--- ex085.py
+++ ex085.py
@@ -1,1 +1,13 @@
+núm = [[], []]
+valor = 0
 
+for c in range(1, 8):
+    valor = int(input('Digite um valor: '))
+    if valor % 2  == 0: núm[0].append(valor)
+    else: núm[1].append(valor)
+
+print('-=' * 30)
+núm[0].sort()
+núm[1].sort()
+print(f'Os números pares são {núm[0]}')
+print(f'Os números impares são: {núm[1]} ')
