9f5adf9b8b935a68d33fdf266261ca51a8a51e9c
Nero1Dev <nairondalmaso207@gmail.com>
2021-10-20 09:53:36 -0400

    ex088

--- ex088.py
+++ ex088.py
@@ -1,4 +1,13 @@
+from random import sample
+from time import sleep
+
 print('-' * 30)
-print('JOGA NA MEGA SENA')
+print('      JOGA NA MEGA SENA      ')
 print('-' * 30)
-quantidade = input('Quantos jogos você quer que eu sorteie? ')
+qtd_de_jogos = int(input('Quantos jogos serão sorteados? '))
+print('-=' * 3, f'SORTEANDO {qtd_de_jogos} JOGOS', '-=' * 3)
+for c in range(qtd_de_jogos):
+    palpite = sorted(sample(range(1, 61), 6))
+    sleep(0.7)
+    print(f'Jogo {c+1}: {palpite}')
+print('-=' * 5, '< BOA SORTE! >', '-=' * 5)
