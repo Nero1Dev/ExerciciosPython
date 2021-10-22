9b03658d473257039504d75f4ede8b8b327c6ffd
Nero1Dev <nairondalmaso2017@gmail.com>
2021-10-22 13:18:36 -0400

    ex089

--- ex089.py
+++ ex089.py
@@ -1,4 +1,3 @@
-temp = list()
 dados = list()
 
 while True:
@@ -7,14 +6,23 @@ while True:
     nota2 = float(input('2º Nota: '))
     resp = str(input('Quer continuar? [S/N] '))
     média = (nota1 + nota2) / 2
-    temp.append(nome)
-    temp.append(nota1)
-    temp.append(nota2)
-    temp.append(média)
-    dados.append(temp[:])
-    temp.clear()
+    dados.append([nome, [nota1, nota2], média])
+
     if resp in 'Nn':
         break
-        
-print(temp)
-print(dados)
+
+print('-=' * 30)
+
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(dados):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('Finalizando...')
+        break
+    if opc <= len(dados) -1:
+        print(f'As notas de {dados[opc][0]} são {dados[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
