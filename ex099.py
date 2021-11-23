b84d6c596cd69b4c6aa5608189240d5bbf5f7f9b
Nero1Dev <nairondalmaso2017@gmail.com>
2021-11-22 20:41:19 -0400

    ex099

---
+++ ex099.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+
+def maior(*valores):
+    print('-=' * 30)
+    print('Analisando os valores passados...')
+    for c in valores:
+        print(c, end=' ')
+        sleep(0.25)
+    print(f'Foram informados {len(valores)} valores ao todo.')
+    print(f'O maior valor informado foi o '
+          f'{max(valores) if len(valores) != 0 else "0"}.')
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
